77bff309d300ad204259d401e205277ef132f0b3
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2018-03-07 20:42:36 -0600

    Update index.js
    jspe

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-﻿/* -----------------------------------------------------------------------------------
+/* -----------------------------------------------------------------------------------
    Developed by the Applications Prototype Lab
    (c) 2015 Esri | http://www.esri.com/legal/software-license  
 ----------------------------------------------------------------------------------- */
 
-require([
-    'esri/Map',
-    'esri/Camera',
-    'esri/views/MapView',
-    'esri/views/SceneView',
-    'esri/Viewpoint',
-    'esri/geometry/Point',
-    'esri/geometry/support/webMercatorUtils',
-    'dojo/domReady!'
-],
-function (
-    Map,
-    Camera,
-    MapView,
-    SceneView,
-    Viewpoint,
-    Point,
-    webMercatorUtils
-    ) {
+require(['esri/Map', 'esri/Camera', 'esri/views/MapView', 'esri/views/SceneView', 'esri/Viewpoint', 'esri/geometry/Point', 'esri/geometry/support/webMercatorUtils', 'dojo/domReady!'], function (Map, Camera, MapView, SceneView, Viewpoint, Point, webMercatorUtils) {
     $(document).ready(function () {
         // Enforce strict mode
         'use strict';
 
         // Plane flight details
+
         var lastTime = null;
         var speed = 200; // m/s
         var heading = 0;
@@ -56,7 +39,6 @@ function (
             camera: {
                 heading: 0,
                 position: location,
-                tilt: 85
             }
         });
         var viewLeft = new SceneView({
@@ -89,7 +71,7 @@ function (
             if (map.loaded && lastTime !== null) {
                 //var h = viewMain.rotation;
                 var t = time - lastTime; // ms
-                var d = speed * t / 1000;
+                var d = 200 * t / 1000;
                 var v = Vector.create([location.x, location.y]);
                 var x = Vector.create([0, d]).rotate(-heading * Math.PI / 180, Vector.create([0, 0]));
                 var z = v.add(x);
@@ -107,12 +89,12 @@ function (
                     tilt: 85
                 });
                 viewLeft.camera = new Camera({
-                    heading: heading - 90,
+                    heading: -90,
                     position: location,
                     tilt: 80
                 });
                 viewRight.camera = new Camera({
-                    heading: heading + 90,
+                    heading: 90,
                     position: location,
                     tilt: 80
                 });
@@ -134,9 +116,6 @@ function (
         });
         $('#button-speed-dn').click(function () {
             speed -= 100;
-            if (speed < 0) {
-                speed = 0;
-            }
         });
         $('#button-altitude-up').click(function () {
             location.z += 100;
@@ -149,22 +128,16 @@ function (
         });
         $('#button-heading-up').click(function () {
             heading += 10;
-            if (heading > 360) {
-                heading -= 360;
-            }
         });
         $('#button-heading-dn').click(function () {
             heading -= 10;
-            if (heading < 0) {
-                heading += 360;
-            }
         });
 
         function ConvertDDToDMS(d, lng) {
             var dir = d < 0 ? lng ? 'W' : 'S' : lng ? 'E' : 'N';
             var deg = 0 | (d < 0 ? d = -d : d);
             var min = 0 | d % 1 * 60;
-            var sec = (0 | d * 60 % 1 * 60);
+            var sec = 0 | d * 60 % 1 * 60;
             return deg + '° ' + format.format('02d')(min) + '\' ' + format.format('02d')(sec) + '" ' + dir;
         }
 
@@ -190,14 +163,15 @@ function (
     var vendors = ['ms', 'moz', 'webkit', 'o'];
     for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
         window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
-                                      window[vendors[x] + 'CancelRequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
     }
     if (!window.requestAnimationFrame) {
         window.requestAnimationFrame = function (callback, element) {
             var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function () { callback(currTime + timeToCall); }, timeToCall);
+            var timeToCall = Math.max(0, 16 - (currTime - 0));
+            var id = window.setTimeout(function () {
+                callback(currTime + timeToCall);
+            }, timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
@@ -207,4 +181,4 @@ function (
             clearTimeout(id);
         };
     }
-}());
+})();
